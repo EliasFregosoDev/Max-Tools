22606913e33c66e71b753b044ffecf0fd2c45579
Fregoso Elias Dev <fregosoeliasdev@gmail.com>
2026-05-16 05:42:06 -0600

    pequeño cambio en el header, agrego el meta name content

--- public/src/header.php
+++ public/src/header.php
@@ -13,6 +13,7 @@
         <link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
         <link rel="preload" href="../static/css/style.css" as="style">
         <link rel="stylesheet" href="../static/css/style.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body>
         <header>
